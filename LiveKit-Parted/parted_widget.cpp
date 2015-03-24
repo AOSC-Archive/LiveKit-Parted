bfc62de63dc964649278917bf8e3ed978e9b8690
Trickness <sternwzhang@outlook.com>
2015-03-24 13:56:08 -0400

    开始搞鼠标事件，尽可能作漂亮点

--- LiveKit-Parted/parted_widget.cpp
+++ LiveKit-Parted/parted_widget.cpp
@@ -4,6 +4,7 @@
 partition_item::partition_item(QWidget *parent):
     QWidget(parent){
     this->Title     = new QLabel(this);
+    this->setMouseTracking(true);
 
     this->Title->setText(tr("Title desu"));
     this->setStyleSheet("background-color:green;");
@@ -13,25 +14,48 @@ partition_item::~partition_item(){
     delete  this->Title;
 }
 
+void partition_item::mouseMoveEvent(QMouseEvent *Event){
+        this->setStyleSheet("background-color:white");
+}
+
+void partition_item::mousePressEvent(QMouseEvent *){
+    //this->setStyleSheet("background-color:blue");
+}
+
 
 
 parted_widget::parted_widget(QWidget *parent):
     QWidget(parent){
     MainLayout      = new QVBoxLayout;
-    ItemsMap        = new partitions_map;
+    PartitionsMap   = new p_map_t;
 
     this->resize(300,100);
     this->setLayout(MainLayout);
 
-
     this->MainLayout->setSpacing(0);
-    this->MainLayout->addWidget(new partition_item());
-    this->MainLayout->addWidget(new partition_item());
-    this->MainLayout->addWidget(new partition_item());
-    this->MainLayout->addWidget(new partition_item());
-    this->MainLayout->addWidget(new partition_item());
+    this->InsertPartitions(new partition_item);
 }
 
 parted_widget::~parted_widget(){
     delete  this->MainLayout;
+    if(this->PartitionsMap->isEmpty() == false){
+        p_map_t::iterator i = this->PartitionsMap->begin();
+        while(i != this->PartitionsMap->end()){
+            delete i.value();
+            i++;
+        }
+        this->PartitionsMap->clear();
+    }
+    delete  this->PartitionsMap;
+}
+
+
+/* If the 'order' is -1, then insert this item to the bottom */
+void parted_widget::InsertPartitions(partition_item *Item,int order){
+    if (order == -1)
+        order = this->PartitionsMap->size();
+    this->PartitionsMap->insert(order,Item);
+    this->MainLayout->insertWidget(order,Item);
+    this->resize(this->width(),this->height()+PARTITION_HEIGTH);
+    return;
 }
--- LiveKit-Parted/parted_widget.h
+++ LiveKit-Parted/parted_widget.h
@@ -5,9 +5,13 @@
 #include <QScrollArea>
 #include <QHBoxLayout>
 #include <QVBoxLayout>
+#include <QMouseEvent>
 #include <QLabel>
 #include <QWidget>
 #include <QMap>
+
+#define PARTED_WIDGET_WIDTH     300
+#define PARTITION_HEIGTH        50
 
 
 class partition_item : public QWidget{
@@ -15,21 +19,24 @@ class partition_item : public QWidget{
 public:
     explicit partition_item(QWidget *parent = 0);
     ~partition_item();
+    void mouseMoveEvent(QMouseEvent *Event);
+    void mousePressEvent(QMouseEvent *);
 private:
     QLabel      *Title;
 };
 
-typedef QMap<int,partition_item*> partitions_map;
+typedef QMap<int,partition_item*> p_map_t;
 
 class parted_widget : public QWidget{
     Q_OBJECT
 public:
-    explicit parted_widget(QWidget *parent = 0);
+    explicit    parted_widget(QWidget *parent = 0);
     ~parted_widget();
+    void        InsertPartitions(partition_item* Item,int order = -1);
 private:
     QVBoxLayout     *MainLayout;
     partition_item  *Item;
-    partitions_map  *ItemsMap;
+    p_map_t         *PartitionsMap;
 };
 
 #endif // PARTED_WIDGET_H
